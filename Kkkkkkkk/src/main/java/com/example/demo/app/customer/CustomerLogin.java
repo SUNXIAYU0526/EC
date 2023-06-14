8afd78f72c2d99063ab2b3dd0293aa75cc85d788
unknown <sunxiayu0526@gmail.com>
2023-06-14 15:17:35 +0900

    commit v2

---
+++ Kkkkkkkk/src/main/java/com/example/demo/app/customer/CustomerLogin.java
@@ -0,0 +1,24 @@
+package com.example.demo.app.customer;
+
+public class CustomerLogin {
+	
+	private String mailaddress;
+	private String password;
+	
+	
+	public String getMailaddress() {
+		return mailaddress;
+	}
+	public void setMailaddress(String mailaddress) {
+		this.mailaddress = mailaddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+
+}
